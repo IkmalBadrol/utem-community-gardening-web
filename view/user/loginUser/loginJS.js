ddd0db20f185fb13decf494fccb96a9e969006f9
ashliya30 <b032110213@student.utem.edu.my>
2024-06-07 03:47:11 +0800

    register program and cancel program

--- view/user/loginUser/loginJS.js
+++ view/user/loginUser/loginJS.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Matric Number:', matricNumber);
         console.log('Password betul:', password);
 
-        fetch('../../../api/loginUser.php', {
+        fetch('http://localhost/utem-community-gardening/api/loginUser.php', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
